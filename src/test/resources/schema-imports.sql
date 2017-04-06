6d9849e2870e45d9fc38d812d93df90c0a4e24ff
seonbo.shim <seonbo.shim@SeonBoShim.hwk.net>
2017-04-06 18:41:49 +0900

    ChiwooWebSchemaTests 스키마 생성 및 데이터 로더 추가

--- src/test/resources/schema-imports.sql
+++ src/test/resources/schema-imports.sql
@@ -1,4 +1,13 @@
-insert into COM_I18N_MESSAGE ( code, message, locale ) values ( 'validation.code.cdId', '''코드 아이디''는 필수 입력 항목 입니다.', 'ko' );
-insert into COM_I18N_MESSAGE ( code, message, locale ) values ( 'validation.code.cdName', '''코드 명''은 필수 입력 항목 입니다.', 'ko' );
-insert into COM_I18N_MESSAGE ( code, message, locale ) values ( 'validation.code.cdVal', '''코드 값''은 필수 입력 항목 입니다.', 'ko' );
-insert into COM_I18N_MESSAGE ( code, message, locale ) values ( 'validation.code.ordno', '''정렬 순번''은 숫자 형식의 필수 입력 항목 입니다.', 'ko' );
+
+INSERT INTO `COM_I18N_MESSAGE` (`code`, `locale`, `message`, `use_yn` ) VALUES
+    ('ui.app.name', 'en', 'App Name', 1 ),
+    ('ui.app.name', 'ko', '앱 이름', 1 ),
+    ('validation.Code.cdId', 'ko', '\'코드 아이디\'는 필수 입력 항목 입니다.', 1 ),
+    ('validation.Code.cdName', 'ko', '\'코드 명\'은 필수 입력 항목 입니다.', 1 ),
+    ('validation.Code.cdVal', 'ko', '\'코드 값\'은 필수 입력 항목 입니다.', 1 ),
+    ('validation.Code.ordno', 'ko', '\'정렬 순번\'은 숫자 형식의 필수 입력 항목 입니다.', 1 ),
+    ('validation.Code.cdId', 'en', '\'cdId\' is may not be null', 1 ),
+    ('validation.Code.cdName', 'en', '\'cdName\' is may not be null', 1 ),
+    ('validation.Code.cdVal', 'en', '\'cdVal\' is may not be null', 1 ),
+    ('validation.Code.ordno', 'en', '\'ordno\' is may not be null and data type is number', 1 )
+    ;
